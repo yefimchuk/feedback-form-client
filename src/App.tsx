80a663b383e90bc6797a205477380576650f26ba
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-06-28 10:01:21 +0300

    setting up env

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAllMessages } from "./BLL/allMessages/allMessages.selector";
-import { fetchMessages } from "./BLL/allMessages/allMessages.slice";
-import { AppWrapper, FlexRow } from "./AppStyledComponent";
+import {useDispatch, useSelector} from "react-redux";
+import {selectAllMessages} from "./BLL/allMessages/allMessages.selector";
+import {fetchMessages} from "./BLL/allMessages/allMessages.slice";
+import {AppWrapper, FlexRow} from "./AppStyledComponent";
 import Footer from "./UI/components/Footer/Footer";
 import Decor from "./UI/components/Decoration/Decoration";
 import FeedbackFormPage from "./UI/pages/FeedbackPage";
 import background from "../src/assets/back.jpg";
-function App() {
-  const dispatch = useDispatch();
-  const { dataMessages } = useSelector(selectAllMessages);
-  useEffect(() => {
-    dispatch(fetchMessages({}));
-  }, []);
-useEffect(()=> {
-    console.log("messagesData", dataMessages);
-},[dataMessages])
 
-  return (
-    <AppWrapper background={background}>
-      <FlexRow>
-        <FeedbackFormPage />
-        <Decor />
-      </FlexRow>
-      <Footer />
-    </AppWrapper>
-  );
+function App() {
+    const dispatch = useDispatch();
+    const {dataMessages} = useSelector(selectAllMessages);
+    useEffect(() => {
+        dispatch(fetchMessages({}));
+    }, []);
+
+    useEffect(() => {
+        console.log("messagesData", dataMessages);
+    }, [dataMessages])
+
+    return (
+        <AppWrapper background={background}>
+            <FlexRow>
+                <FeedbackFormPage/>
+                <Decor/>
+            </FlexRow>
+            <Footer/>
+        </AppWrapper>
+    );
 }
 
 export default App;
